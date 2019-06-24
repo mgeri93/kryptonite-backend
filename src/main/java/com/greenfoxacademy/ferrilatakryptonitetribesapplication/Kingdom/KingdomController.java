989b062da590e4de9ff616d20580d9361c93a905
Andras Knuth <knuth.andras89@gmail.com>
2019-06-24 16:35:57 +0200

    fix(KingdomCont, UserCont): remove unnecessary Autowires

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/Kingdom/KingdomController.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/Kingdom/KingdomController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.Kingdom;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KingdomController {
-
-  KingdomRepository kingdomRepository;
-
-  public KingdomController(KingdomRepository kingdomRepository) {
-    this.kingdomRepository = kingdomRepository;
-  }
 
   @GetMapping("/kingdom")
   ResponseEntity<String> kingdom(){
--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/User/UserController.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/User/UserController.java
@@ -1,4 +1,5 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,20 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-  UserRepository userRepository;
 
-  public UserController(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
 
   @GetMapping("/login")
   ResponseEntity<String> login() {
     return new ResponseEntity<>("login", HttpStatus.OK);
-  }
-
-  @GetMapping("/hello")
-  ResponseEntity<String> hello() {
-    return new ResponseEntity<>("Hello World!", HttpStatus.OK);
   }
 
   @GetMapping("/register")
