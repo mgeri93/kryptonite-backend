026e35e281b11297333dbf83b023190f4abbbacb
Daniel Fodor <daniel.fodor91@gmail.com>
2019-06-24 14:04:02 +0200

    changed gradle file too v2

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/FerrilataKryptoniteTribesApplication.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/FerrilataKryptoniteTribesApplication.java
@@ -9,5 +9,4 @@ public class FerrilataKryptoniteTribesApplication {
   public static void main(String[] args) {
     SpringApplication.run(FerrilataKryptoniteTribesApplication.class, args);
   }
-
 }
