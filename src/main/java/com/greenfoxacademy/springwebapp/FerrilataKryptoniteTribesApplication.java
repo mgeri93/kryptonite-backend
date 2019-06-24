1d115057698f3433a15c757db3da9f54e5204ba9
Daniel Fodor <daniel.fodor91@gmail.com>
2019-06-24 13:12:21 +0200

    renamed application

--- src/main/java/com/greenfoxacademy/springwebapp/FerrilataKryptoniteTribesApplication.java
+++ src/main/java/com/greenfoxacademy/springwebapp/FerrilataKryptoniteTribesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebappApplication {
+public class FerrilataKryptoniteTribesApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringWebappApplication.class, args);
+    SpringApplication.run(FerrilataKryptoniteTribesApplication.class, args);
   }
 
 }
