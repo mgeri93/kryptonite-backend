79c1f7a163b332876482016e82e4f00a2f0b7bc2
Daniel Fodor <daniel.fodor91@gmail.com>
2019-06-25 15:39:22 +0200

    modified service + test

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/Building/BuildingServiceTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/Building/BuildingServiceTest.java
@@ -1,15 +1,30 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.Building;
 
-import org.junit.Test;
-import org.mockito.Mock;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class BuildingServiceTest {
 
   private BuildingService buildingService;
 
   @Mock private BuildingRepository buildingRepository;
 
-  private Building mine = new Mine();
+  private Building academy = new Academy();
+
+  @Test
+  public void validBuilding_ReturnsTrue() {
+    assertTrue(buildingService.isValidBuilding(academy));
+  }
+
+  @Test
+  public void isValidBuildingWithIncorrectInput() {}
 
   @Test
   public void saveValidBuilding() {}
