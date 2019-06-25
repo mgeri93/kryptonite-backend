e995ea2278757a3fe2ca53025defcbbf10b14b98
Andras Knuth <knuth.andras89@gmail.com>
2019-06-25 13:36:06 +0200

    feat(User): add User class and repo

---
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/User/UserRepository.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.ferrilatakryptonitetribesapplication.User;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
