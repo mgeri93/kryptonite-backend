be91cbd5bf45678560ed704bfebd5f64e5953df5
mGeri <gerasz01@gmail.com>
2019-06-25 13:39:49 +0200

    feat(UserService)

---
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/User/UserService.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.ferrilatakryptonitetribesapplication.User;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  private UserRepository userRepository;
+
+  @Autowired
+  public UserService(UserRepository userRepository){
+    this.userRepository = userRepository;
+  }
+
+  public boolean isValidUser(User user){
+    return user.getUsername() != null;
+  }
+
+  public void saveUser(User user){
+    userRepository.save(user);
+  }
+
+  public Optional<User> findById(long id){
+    return userRepository.findById(id);
+  }
+
+}
