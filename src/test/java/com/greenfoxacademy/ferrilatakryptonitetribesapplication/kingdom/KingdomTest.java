e80268b6fa5902f5b6813e84f7cb58ef682f4b56
Attila Fekete <feketeattila89@gmail.com>
2019-06-26 17:04:52 +0200

    FKT13

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomTest.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.kingdom;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/UserTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/UserTest.java
@@ -1,8 +1,7 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.user;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class UserTest {
 
