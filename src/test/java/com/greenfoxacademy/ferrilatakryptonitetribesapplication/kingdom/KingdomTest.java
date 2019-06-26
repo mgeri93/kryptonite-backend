a9b32bf32a113aaa925249f3e8ddb39d4ecf732e
Attila Fekete <feketeattila89@gmail.com>
2019-06-26 16:48:42 +0200

    FKT13

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomTest.java
@@ -19,7 +19,7 @@ public class KingdomTest {
   private KingdomService kingdomService;
 
   @Autowired
-  private IKingdomRepository iKingdomRepository;
+  private IKingdomRepository kingdomRepository;
 
   @Test
   public void isValidKingdomWithCorrectInputs () {
@@ -34,7 +34,7 @@ public class KingdomTest {
   @Test
   public void isExistingKingdomForExisting() {
     Kingdom myKingdom = new Kingdom("Attila", "Tanya");
-    iKingdomRepository.save(myKingdom);
+    kingdomRepository.save(myKingdom);
     assertTrue(kingdomService.isExistingKingdom(myKingdom));
   }
 }
--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/UserTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/UserTest.java
@@ -1,8 +1,8 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.user;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserTest {
 
