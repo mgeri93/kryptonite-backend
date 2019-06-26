03d3b4933818d6e008cd320fb0f468d4ae670bfe
Attila Fekete <feketeattila89@gmail.com>
2019-06-26 16:37:50 +0200

    FKT13

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomTest.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.kingdom;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
