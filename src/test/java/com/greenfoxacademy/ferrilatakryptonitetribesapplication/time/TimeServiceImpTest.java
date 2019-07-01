922f39cf3d888ccfd56162c3d431116400ba8231
Attila Fekete <feketeattila89@gmail.com>
2019-07-01 17:05:06 +0200

    FKT16

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeServiceImpTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeServiceImpTest.java
@@ -4,11 +4,9 @@ import java.sql.Timestamp;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
