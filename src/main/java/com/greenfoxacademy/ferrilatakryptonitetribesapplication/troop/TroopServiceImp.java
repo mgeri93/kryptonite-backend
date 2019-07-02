14328a5b3a7ba3545f05dfe598dab9a3a14a824f
Attila Fekete <feketeattila89@gmail.com>
2019-07-02 10:11:47 +0200

    FKT15

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/troop/TroopServiceImp.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/troop/TroopServiceImp.java
@@ -8,6 +8,6 @@ public class TroopServiceImp implements TroopService {
   @Override
   public boolean isValidTroop(Troop troop) {
     return (troop.getLevel() > 0 && troop.getAttack() > 0 && troop.getDefense() > 0
-    && troop.getHp() > 0 && troop.getKingdom() != null);
+        && troop.getHp() > 0 && troop.getKingdom() != null);
   }
 }
