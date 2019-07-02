f49c48ac9a2d694398c2d450ab3e8c88912be66d
Attila Fekete <feketeattila89@gmail.com>
2019-07-02 10:14:04 +0200

    FKT15

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/troop/TroopTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/troop/TroopTest.java
@@ -1,12 +1,11 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.troop;
 
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.kingdom.Kingdom;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
-
-import com.greenfoxacademy.ferrilatakryptonitetribesapplication.kingdom.Kingdom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.mockito.Mockito.when;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
