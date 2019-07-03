9071b3986b63b32b26477012024b22864ad390c3
Attila Fekete <feketeattila89@gmail.com>
2019-07-03 10:28:23 +0200

    FKT16

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeServiceImpTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeServiceImpTest.java
@@ -4,10 +4,9 @@ import java.sql.Timestamp;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,15 +14,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @AutoConfigureMockMvc
 public class TimeServiceImpTest {
 
-  @MockBean
+  @Autowired
   TimeServiceImp timeServiceImp;
 
   @Test
   public void timeLeftWithFutureFinish() {
     String answer = "1970-01-01 01:08:57.975";
     Timestamp testStamp = Timestamp.valueOf(answer);
-    when(timeServiceImp.timeLeft(Timestamp.valueOf("2019-07-02 18:48:05.123"),
-        Timestamp.valueOf("2019-07-02 18:57:03.098"))).thenReturn(testStamp);
     Timestamp myStamp = timeServiceImp.timeLeft(Timestamp.valueOf("2019-07-02 18:48:05.123"),
         Timestamp.valueOf("2019-07-02 18:57:03.098"));
     assertEquals(myStamp, testStamp);
