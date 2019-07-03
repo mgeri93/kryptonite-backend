bbb08de6a9fea8209033c2ed3fb3b87ca28d4055
Attila Fekete <feketeattila89@gmail.com>
2019-07-03 16:07:10 +0200

    FKT15

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/troop/Troop.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/troop/Troop.java
@@ -21,31 +21,42 @@ public class Troop {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
   @NotNull
-  private int level = 1;
-  private int hp = 1;
-  private int attack = 1;
-  private int defense = 1;
+  private int level = 0;
+  private int hp = 0;
+  private int attack = 0;
+  private int defense = 0;
 
   @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn(name = "kingdom_id")
   private Kingdom kingdom;
 
   public Troop() {
+    this.setLevel(1);
+    this.setHp(1);
+    this.setAttack(1);
+    this.setDefense(1);
   }
 
-  public Troop(@NotNull int level, int hp, int attack, int defense,
-      Kingdom kingdom) {
-    if (level > 0) {
+  public Troop(int level, int hp, int attack, int defense) {
+    if (level > 1) {
       this.level = level;
+    } else {
+      this.level = 1;
     }
-    if (hp > 0) {
+    if (hp > 1) {
       this.hp = hp;
+    } else {
+      this.hp = 1;
     }
-    if (attack > 0) {
+    if (attack > 1) {
       this.attack = attack;
+    } else {
+      this.attack = 1;
     }
-    if (defense > 0) {
+    if (defense > 1) {
       this.defense = defense;
+    } else {
+      this.defense = 1;
     }
   }
 }
