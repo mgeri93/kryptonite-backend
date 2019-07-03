bb6574c96a15264720c95b934db2c936783bb9a5
mGeri <gerasz01@gmail.com>
2019-07-03 10:02:42 +0200

    feat(correctTest)

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/UserTest.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/UserTest.java
@@ -60,7 +60,7 @@ public class UserTest {
 
   @Test
   public void isExistingUser() {
-    User testUser = new User("g eri", "password");
+    User testUser = new User("geri", "password");
     assertFalse(userService.isExistingUser(testUser));
   }
 
