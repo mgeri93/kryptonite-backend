898365fb44d1156e67d4b1b1896644cf2a06ba92
Andras Knuth <knuth.andras89@gmail.com>
2019-07-08 16:02:58 +0200

    feat(project):implement jwt token generation

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/security/WebSecurity.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/security/WebSecurity.java
@@ -1,5 +1,52 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.security;
 
-public class WebSecurity {
+import static com.greenfoxacademy.ferrilatakryptonitetribesapplication.security.SecurityConstants.REGISTER_URL;
 
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.user.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+  private UserDetailsServiceImpl userDetailsServiceImpl;
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  public WebSecurity(UserDetailsServiceImpl userDetailsServiceImpl, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.userDetailsServiceImpl = userDetailsServiceImpl;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable().authorizeRequests()
+        .antMatchers(HttpMethod.POST, REGISTER_URL).permitAll()
+        .anyRequest().authenticated()
+        .and()
+        //.antMatcher("/login", )
+        .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+        .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+        // this disables session creation on Spring Security
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(bCryptPasswordEncoder);
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
+  }
 }
--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/ApplicationUserController.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/ApplicationUserController.java
@@ -3,19 +3,22 @@ package com.greenfoxacademy.ferrilatakryptonitetribesapplication.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RequestMapping("/users")
+
+
 @RestController
 public class ApplicationUserController {
-
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
   private ApplicationUserServiceImpl userService;
 
   @Autowired
-  public ApplicationUserController(ApplicationUserServiceImpl userService) {
+  public ApplicationUserController(ApplicationUserServiceImpl userService,
+      BCryptPasswordEncoder bCryptPasswordEncoder) {
     this.userService = userService;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
   }
 
   /*@PostMapping("/login")
@@ -32,6 +35,7 @@ public class ApplicationUserController {
 
   @PostMapping("/register")
   ResponseEntity register(@RequestBody ApplicationUserDTO applicationUserDTO) {
+    applicationUserDTO.setPassword(bCryptPasswordEncoder.encode(applicationUserDTO.getPassword()));
     return userService.registerNewUser(applicationUserDTO);
   }
 }
