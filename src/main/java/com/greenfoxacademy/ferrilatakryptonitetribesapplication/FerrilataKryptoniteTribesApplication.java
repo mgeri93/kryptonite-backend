af5e620502749922df9f47392df9ca444336abc0
Andras Knuth <knuth.andras89@gmail.com>
2019-07-08 13:45:25 +0200

    refactor(application): rename User to ApplicationUser

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/FerrilataKryptoniteTribesApplication.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/FerrilataKryptoniteTribesApplication.java
@@ -2,11 +2,18 @@ package com.greenfoxacademy.ferrilatakryptonitetribesapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FerrilataKryptoniteTribesApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(FerrilataKryptoniteTribesApplication.class, args);
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
---
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/user/ApplicationUser.java
@@ -0,0 +1,30 @@
+package com.greenfoxacademy.ferrilatakryptonitetribesapplication.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ApplicationUser {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String username;
+  private String password;
+
+  public ApplicationUser() {
+
+  }
+
+  public ApplicationUser(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
+}
