9e93944601d1e90d87aca7aa627e3644e057561a
Daniel Fodor <daniel.fodor91@gmail.com>
2019-07-10 11:29:39 +0200

    created correct method with tests

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeService.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeService.java
@@ -4,4 +4,10 @@ import java.sql.Timestamp;
 
 interface TimeService {
   Timestamp timeLeft(Timestamp start, Timestamp finish) throws Exception;
+
+  long getCurrentTime();
+
+  Timestamp calculateCompletionTimeOfTroop(long troopId);
+
+  Timestamp calculateCompletionTimeOfBuilding(long buildingId);
 }
--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeServiceImp.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/time/TimeServiceImp.java
@@ -1,10 +1,24 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.time;
 
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.building.BuildingServiceImpl;
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.troop.TroopServiceImp;
 import java.sql.Timestamp;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TimeServiceImp implements TimeService {
+
+  private TroopServiceImp troopService;
+
+  private BuildingServiceImpl buildingService;
+
+  @Autowired
+  public TimeServiceImp(TroopServiceImp troopService, BuildingServiceImpl buildingService) {
+    this.troopService = troopService;
+    this.buildingService = buildingService;
+  }
 
   @Override
   public Timestamp timeLeft(Timestamp start, Timestamp finish) throws Exception {
@@ -12,5 +26,22 @@ public class TimeServiceImp implements TimeService {
       throw new Exception("Start time is greater than finish time!");
     }
     return new Timestamp(finish.getTime() - start.getTime());
+  }
+
+  @Override
+  public long getCurrentTime() {
+    return new Timestamp(System.currentTimeMillis()).getTime();
+  }
+
+  @Override
+  public Timestamp calculateCompletionTimeOfTroop(long troopId) {
+    long completionTime = troopService.findTroopById(troopId).getLevel();
+    return new Timestamp(getCurrentTime() + TimeUnit.MINUTES.toMillis(completionTime));
+  }
+
+  @Override
+  public Timestamp calculateCompletionTimeOfBuilding(long buildingId) {
+    long completionTime = buildingService.findBuildingById(buildingId).getLevel() * 5;
+    return new Timestamp(getCurrentTime() + TimeUnit.MINUTES.toMillis(completionTime));
   }
 }
