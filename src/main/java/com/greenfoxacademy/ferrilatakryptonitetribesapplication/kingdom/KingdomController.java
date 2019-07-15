165d984b64292a2f12f06a94b67d9b41900f6c71
mGeri <gerasz01@gmail.com>
2019-07-15 13:46:21 +0200

    feat(preapareForEndpoints)

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomController.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomController.java
@@ -4,9 +4,11 @@ import com.greenfoxacademy.ferrilatakryptonitetribesapplication.resource.Resourc
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kingdom")
 public class KingdomController {
 
   private ResourceServiceImpl resourceService;
@@ -17,7 +19,7 @@ public class KingdomController {
     this.kingdomService = kingdomService;
   }
 
-  @GetMapping("/kingdom")
+  @GetMapping("")
   ResponseEntity<String> kingdom() throws Exception {
     resourceService.refresh(kingdomService.findKingdomById(1).getResourceList().get(0));
     return new ResponseEntity<>("kingdom", HttpStatus.OK);
