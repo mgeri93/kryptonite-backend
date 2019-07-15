616cef8035adf5744ac4a45c88ff6d89e03cdddb
Attila Fekete <feketeattila89@gmail.com>
2019-07-15 16:15:59 +0200

    feat(KingdomController) /{id}/resources endpont created

--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/IKingdomRepository.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/IKingdomRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface IKingdomRepository extends CrudRepository<Kingdom, Long> {
 
   public boolean existsByName(String name);
+  Kingdom findById(long id);
 }
--- src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomController.java
+++ src/main/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/kingdom/KingdomController.java
@@ -1,15 +1,38 @@
 package com.greenfoxacademy.ferrilatakryptonitetribesapplication.kingdom;
 
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.applicationuser.ApplicationUserRepository;
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.exception.customexceptions.KingdomRelatedException;
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.exception.customexceptions.ResourceRelatedException;
+import com.greenfoxacademy.ferrilatakryptonitetribesapplication.resource.Resource;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kingdom")
 public class KingdomController {
+
+  private IKingdomRepository kingdomRepository;
+
+  public KingdomController(IKingdomRepository kingdomRepository) {
+    this.kingdomRepository = kingdomRepository;
+  }
 
   @GetMapping("/kingdom")
   ResponseEntity<String> kingdom() {
     return new ResponseEntity<>("kingdom", HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}/resources")
+  List<Resource> getKingdomResources(@PathVariable (name = "id") long id) {
+    if (kingdomRepository.existsById(id)) {
+      return kingdomRepository.findById(id).getResourceList();
+    }
+    throw new KingdomRelatedException("No kingdom exists with this id");
+
   }
 }
