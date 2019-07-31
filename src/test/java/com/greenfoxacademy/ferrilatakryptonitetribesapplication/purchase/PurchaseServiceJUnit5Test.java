71f8bc39a48c4aa7909af0492565821ad6d4a8ea
Attila Fekete <feketeattila89@gmail.com>
2019-07-31 13:01:42 +0200

    feat(tests) removed JUnit4 imports from test class

--- src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/purchase/PurchaseServiceJUnit5Test.java
+++ src/test/java/com/greenfoxacademy/ferrilatakryptonitetribesapplication/purchase/PurchaseServiceJUnit5Test.java
@@ -14,13 +14,11 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class PurchaseServiceJUnit5Test {
